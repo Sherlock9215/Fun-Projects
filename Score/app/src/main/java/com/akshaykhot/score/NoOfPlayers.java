dd12f93b40f04e3787fffb5047e2f2d362d9dd0d
Akshay Khot <akshay.7277@gmail.com>
2015-10-17 14:03:37 -0700

    start the project

---
+++ Score/app/src/main/java/com/akshaykhot/score/NoOfPlayers.java
@@ -0,0 +1,36 @@
+package com.akshaykhot.score;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class NoOfPlayers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Home.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the acttivity layout
+        setContentView(textView);
+
+    }
+
+
+
+}
