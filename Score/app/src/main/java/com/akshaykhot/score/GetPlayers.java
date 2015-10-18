a502cd041030729518ed862406a6c0a7a4847bc5
Akshay Khot <akshay.7277@gmail.com>
2015-10-18 14:43:18 -0700

    Add Dynamic Layout

--- Score/app/src/main/java/com/akshaykhot/score/GetPlayers.java
+++ Score/app/src/main/java/com/akshaykhot/score/GetPlayers.java
@@ -1,5 +1,6 @@
 package com.akshaykhot.score;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,23 @@ public class GetPlayers extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_get_players);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        int numPlayers = intent.getIntExtra("Number of Players", 2);
+
+        // Display proper view depending on numPlayers
+        switch (numPlayers) {
+            case 2:
+                setContentView(R.layout.two_players);
+                break;
+            case 3:
+                setContentView(R.layout.three_players);
+                break;
+            case 4:
+                setContentView(R.layout.four_players);
+                break;
+        }
+
     }
 }
---
+++ Score/app/src/main/java/com/akshaykhot/score/Winner.java
@@ -0,0 +1,13 @@
+package com.akshaykhot.score;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Winner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_winner);
+    }
+}
