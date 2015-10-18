4e4bee11d3379bdd78e5c40a7351a0e62c97f761
Akshay Khot <akshay.7277@gmail.com>
2015-10-17 23:03:31 -0700

    display numPlayers in next activity

--- Score/app/src/main/java/com/akshaykhot/score/NoOfPlayers.java
+++ Score/app/src/main/java/com/akshaykhot/score/NoOfPlayers.java
@@ -7,6 +7,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -16,19 +18,39 @@ public class NoOfPlayers extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_of_players);
+    }
 
-        // Get the message from the intent
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(Home.EXTRA_MESSAGE);
+    /**
+     * called when the user clicks the appropriate button
+     */
+    public void sendNumPlayers(View view) {
 
-        // Create the text view
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
+        // variable to store number of players(default is 2)
+        int numPlayers = 2;
 
-        // Set the text view as the acttivity layout
-        setContentView(textView);
+        // Find out which button was clicked, from the button(which is a view) id
+        switch (view.getId()) {
+            case R.id.button2:
+                Button button2 = (Button) findViewById(R.id.button2);
+                numPlayers = 2;
+                break;
+            case R.id.button3:
+                Button button3 = (Button) findViewById(R.id.button3);
+                numPlayers = 3;
+                break;
+            case R.id.button4:
+                Button button4 = (Button) findViewById(R.id.button4);
+                numPlayers = 4;
+                break;
+        }
 
+        // Create a new intent to start new activity with the given data
+        Intent intent = new Intent(this, PlayerNames.class);
+        // Put the numPlayers in the intent
+        intent.putExtra("Number of Players", numPlayers);
+        // Start the second activity
+        startActivity(intent);
     }
 
 
---
+++ Score/app/src/main/java/com/akshaykhot/score/PlayerNames.java
@@ -0,0 +1,49 @@
+package com.akshaykhot.score;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PlayerNames extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_names);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        int numPlayers = intent.getIntExtra("Number of Players", 4);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText("No. of Players: " + numPlayers);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
+
+/*
+        // Create the appropriate display depending on number of players
+        switch (numPlayers) {
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+        }
+*/
+    }
+
+
+}
