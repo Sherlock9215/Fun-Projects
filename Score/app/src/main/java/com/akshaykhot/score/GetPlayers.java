193a0edf3becd804bcb9880da4a37676c6c06e85
Akshay Khot <akshay.7277@gmail.com>
2015-10-19 19:53:30 -0700

    get input and dynamic playerNames

--- Score/app/src/main/java/com/akshaykhot/score/GetPlayers.java
+++ Score/app/src/main/java/com/akshaykhot/score/GetPlayers.java
@@ -3,8 +3,13 @@ package com.akshaykhot.score;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class GetPlayers extends AppCompatActivity {
+
+    int numPlayers;
+    String player1, player2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +17,7 @@ public class GetPlayers extends AppCompatActivity {
 
         // Get the message from the intent
         Intent intent = getIntent();
-        int numPlayers = intent.getIntExtra("Number of Players", 2);
+        numPlayers = intent.getIntExtra("Number of Players", 2);
 
         // Display proper view depending on numPlayers
         switch (numPlayers) {
@@ -27,5 +32,37 @@ public class GetPlayers extends AppCompatActivity {
                 break;
         }
 
+    }
+
+    public void startGame(View view) {
+        // Create a new intent to start new activity with the given data
+        Intent intent = new Intent(this, TrackScore.class);
+        // Put the numPlayers in the intent
+        intent.putExtra("Number of Players", numPlayers);
+
+        // Put the player names in the intent
+        switch (numPlayers) {
+            case 2:
+                EditText playerOneEditText = (EditText) findViewById(R.id.editText);
+                player1 = playerOneEditText.getText().toString();
+                intent.putExtra("Player One", player1);
+
+                EditText playerTwoEditText = (EditText) findViewById(R.id.editText2);
+                player2 = playerTwoEditText.getText().toString();
+                intent.putExtra("Player Two", player2);
+
+                break;
+            case 3:
+                setContentView(R.layout.three_players);
+                break;
+            case 4:
+                setContentView(R.layout.four_players);
+                break;
+        }
+
+
+
+        // Start the second activity
+        startActivity(intent);
     }
 }
--- Score/app/src/main/java/com/akshaykhot/score/TrackScore.java
+++ Score/app/src/main/java/com/akshaykhot/score/TrackScore.java
@@ -1,13 +1,46 @@
 package com.akshaykhot.score;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class TrackScore extends AppCompatActivity {
+
+    int numPlayers;
+    String player1, player2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_track_score);
+
+        // Get the number of players and their names from the intent
+        Intent intent = getIntent();
+        numPlayers = intent.getIntExtra("Number of Players", 2);
+        player1 = intent.getStringExtra("Player One");
+        player2 = intent.getStringExtra("Player Two");
+
+        // Display proper view depending on numPlayers
+        switch (numPlayers) {
+            case 2:
+                setContentView(R.layout.two_score);
+
+                // Get a handle on the TextViews for player names
+                TextView playerOneTextView = (TextView) findViewById(R.id.textView8);
+                TextView playerTwoTextView = (TextView) findViewById(R.id.textView9);
+                // Set the text(names)
+                playerOneTextView.setText(player1);
+                playerTwoTextView.setText(player2);
+                break;
+            case 3:
+                setContentView(R.layout.three_score);
+                break;
+            case 4:
+                setContentView(R.layout.four_score);
+                break;
+        }
     }
+
+
 }
