5d7a3c4eeddfe102af2d917374d5f16e27905c37
Akshay Khot <akshay.7277@gmail.com>
2015-10-23 23:23:31 -0700

    Added support for 3 and 4 players

--- Score/app/src/main/java/com/akshaykhot/score/GetPlayers.java
+++ Score/app/src/main/java/com/akshaykhot/score/GetPlayers.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 public class GetPlayers extends AppCompatActivity {
 
     int numPlayers;
-    String player1, player2;
+    String player1, player2, player3, player4;
 
     @Override
     /**
@@ -43,6 +43,8 @@ public class GetPlayers extends AppCompatActivity {
      * calls the activity to start tracking scores
      */
     public void startGame(View view) {
+
+        EditText playerOneEditText, playerTwoEditText, playerThreeEditText, playerFourEditText;
         // Create a new intent to start new activity with the given data
         Intent intent = new Intent(this, TrackScore.class);
         // Put the numPlayers in the intent
@@ -51,20 +53,46 @@ public class GetPlayers extends AppCompatActivity {
         // Put the player names in the intent
         switch (numPlayers) {
             case 2:
-                EditText playerOneEditText = (EditText) findViewById(R.id.editText);
+                playerOneEditText = (EditText) findViewById(R.id.editText);
                 player1 = playerOneEditText.getText().toString();
                 intent.putExtra("Player One", player1);
 
-                EditText playerTwoEditText = (EditText) findViewById(R.id.editText2);
+                playerTwoEditText = (EditText) findViewById(R.id.editText2);
                 player2 = playerTwoEditText.getText().toString();
                 intent.putExtra("Player Two", player2);
 
                 break;
             case 3:
-                setContentView(R.layout.three_players);
+                playerOneEditText = (EditText) findViewById(R.id.editText);
+                player1 = playerOneEditText.getText().toString();
+                intent.putExtra("Player One", player1);
+
+                playerTwoEditText = (EditText) findViewById(R.id.editText2);
+                player2 = playerTwoEditText.getText().toString();
+                intent.putExtra("Player Two", player2);
+
+                playerThreeEditText = (EditText) findViewById(R.id.editText3);
+                player3 = playerThreeEditText.getText().toString();
+                intent.putExtra("Player Three", player3);
+
                 break;
             case 4:
-                setContentView(R.layout.four_players);
+                playerOneEditText = (EditText) findViewById(R.id.editText);
+                player1 = playerOneEditText.getText().toString();
+                intent.putExtra("Player One", player1);
+
+                playerTwoEditText = (EditText) findViewById(R.id.editText2);
+                player2 = playerTwoEditText.getText().toString();
+                intent.putExtra("Player Two", player2);
+
+                playerThreeEditText = (EditText) findViewById(R.id.editText3);
+                player3 = playerThreeEditText.getText().toString();
+                intent.putExtra("Player Three", player3);
+
+                playerFourEditText = (EditText) findViewById(R.id.editText4);
+                player4 = playerFourEditText.getText().toString();
+                intent.putExtra("Player Four", player4);
+
                 break;
         }
 
--- Score/app/src/main/java/com/akshaykhot/score/TrackScore.java
+++ Score/app/src/main/java/com/akshaykhot/score/TrackScore.java
@@ -12,41 +12,84 @@ import android.widget.TextView;
 
 public class TrackScore extends AppCompatActivity {
 
+    // Number of players
     int numPlayers;
-    String player1, player2;
-    int playerOneScore, playerTwoScore;
+
+    // Player names
+    String player1, player2, player3, player4;
+
+    // PlayerScores
+    int playerOneScore, playerTwoScore, playerThreeScore, playerFourScore;
 
     @Override
     /**
      * get the numPlayers and playerNames from the intent
-     * and displayes the proper view according to numPlayers
+     * and displays the proper view according to numPlayers
      */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Get the number of players and their names from the intent
+        TextView playerOneTextView, playerTwoTextView, playerThreeTextView, playerFourTextView;
+
+        // Get the number of players from the intent
         Intent intent = getIntent();
         numPlayers = intent.getIntExtra("Number of Players", 2);
-        player1 = intent.getStringExtra("Player One");
-        player2 = intent.getStringExtra("Player Two");
 
         // Display proper view depending on numPlayers
         switch (numPlayers) {
             case 2:
                 setContentView(R.layout.two_score);
 
+                // Get the player names
+                player1 = intent.getStringExtra("Player One");
+                player2 = intent.getStringExtra("Player Two");
+
                 // Get a handle on the TextViews for player names
-                TextView playerOneTextView = (TextView) findViewById(R.id.textView8);
-                TextView playerTwoTextView = (TextView) findViewById(R.id.textView9);
+                playerOneTextView = (TextView) findViewById(R.id.textView8);
+                playerTwoTextView = (TextView) findViewById(R.id.textView9);
                 // Set the text(names)
                 playerOneTextView.setText(player1);
                 playerTwoTextView.setText(player2);
+
                 break;
             case 3:
                 setContentView(R.layout.three_score);
+
+                // Get the player names
+                player1 = intent.getStringExtra("Player One");
+                player2 = intent.getStringExtra("Player Two");
+                player3 = intent.getStringExtra("Player Three");
+
+                // Get a handle on the TextViews for player names
+                playerOneTextView = (TextView) findViewById(R.id.textView8);
+                playerTwoTextView = (TextView) findViewById(R.id.textView9);
+                playerThreeTextView = (TextView) findViewById(R.id.textView10);
+                // Set the text(names)
+                playerOneTextView.setText(player1);
+                playerTwoTextView.setText(player2);
+                playerThreeTextView.setText(player3);
+
                 break;
             case 4:
                 setContentView(R.layout.four_score);
+
+                // Get the player names
+                player1 = intent.getStringExtra("Player One");
+                player2 = intent.getStringExtra("Player Two");
+                player3 = intent.getStringExtra("Player Three");
+                player4 = intent.getStringExtra("Player Four");
+
+                // Get a handle on the TextViews for player names
+                playerOneTextView = (TextView) findViewById(R.id.textView8);
+                playerTwoTextView = (TextView) findViewById(R.id.textView9);
+                playerThreeTextView = (TextView) findViewById(R.id.textView10);
+                playerFourTextView = (TextView) findViewById(R.id.textView11);
+                // Set the text(names)
+                playerOneTextView.setText(player1);
+                playerTwoTextView.setText(player2);
+                playerThreeTextView.setText(player3);
+                playerFourTextView.setText(player4);
+
                 break;
         }
     }
@@ -59,40 +102,162 @@ public class TrackScore extends AppCompatActivity {
      */
     public void add(View view) {
 
-        //get the numbers from the text boxes
-        EditText player1Score = (EditText) findViewById(R.id.editText7);
-        int p1 = Integer.parseInt(player1Score.getText().toString());
-        EditText player2Score = (EditText) findViewById(R.id.editText8);
-        int p2 = Integer.parseInt(player2Score.getText().toString());
-
-        // Add the scores into the view
+        //default parameter setup
         LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
                 AbsListView.LayoutParams.MATCH_PARENT,
                 AbsListView.LayoutParams.WRAP_CONTENT, 1.0f);
 
-        LinearLayout leftColumn = (LinearLayout) findViewById(R.id.leftColumn);
-        TextView p1Score = new TextView(this);
-        p1Score.setLayoutParams(param);
-        p1Score.setText(Integer.toString(p1));
-        p1Score.setGravity(Gravity.CENTER);
-        p1Score.setTextSize(20);
-        leftColumn.addView(p1Score);
+        EditText player1Score, player2Score, player3Score, player4Score;
+        int p1, p2, p3, p4;
+        LinearLayout column1, column2, column3, column4;
+        TextView p1Score, p2Score, p3Score, p4Score;
 
-        LinearLayout rightColumn = (LinearLayout) findViewById(R.id.rightColumn);
-        TextView p2Score = new TextView(this);
-        p2Score.setLayoutParams(param);
-        p2Score.setText(Integer.toString(p2));
-        p2Score.setGravity(Gravity.CENTER);
-        p2Score.setTextSize(20);
-        rightColumn.addView(p2Score);
+        switch(numPlayers) {
 
-        // Add the scores to current scores
-        playerOneScore += p1;
-        playerTwoScore += p2;
+            case 2:
 
-        // Make the fields blank for the next input
-        player1Score.setText("");
-        player2Score.setText("");
+                //get the numbers from the text boxes
+                player1Score = (EditText) findViewById(R.id.editText7);
+                p1 = Integer.parseInt(player1Score.getText().toString());
+                player2Score = (EditText) findViewById(R.id.editText8);
+                p2 = Integer.parseInt(player2Score.getText().toString());
+
+                // Add the scores into the view
+                column1 = (LinearLayout) findViewById(R.id.Column1);
+                p1Score = new TextView(this);
+                p1Score.setLayoutParams(param);
+                p1Score.setText(Integer.toString(p1));
+                p1Score.setGravity(Gravity.CENTER);
+                p1Score.setTextSize(20);
+                column1.addView(p1Score);
+
+                column2 = (LinearLayout) findViewById(R.id.Column2);
+                p2Score = new TextView(this);
+                p2Score.setLayoutParams(param);
+                p2Score.setText(Integer.toString(p2));
+                p2Score.setGravity(Gravity.CENTER);
+                p2Score.setTextSize(20);
+                column2.addView(p2Score);
+
+                // Add the scores to current scores
+                playerOneScore += p1;
+                playerTwoScore += p2;
+
+                // Make the fields blank for the next input
+                player1Score.setText("");
+                player2Score.setText("");
+
+                break;
+
+            case 3:
+
+                //get the numbers from the text boxes
+                player1Score = (EditText) findViewById(R.id.editText7);
+                p1 = Integer.parseInt(player1Score.getText().toString());
+                player2Score = (EditText) findViewById(R.id.editText8);
+                p2 = Integer.parseInt(player2Score.getText().toString());
+                player3Score = (EditText) findViewById(R.id.editText9);
+                p3 = Integer.parseInt(player3Score.getText().toString());
+
+                // Add the scores into the view
+                column1 = (LinearLayout) findViewById(R.id.Column1);
+                p1Score = new TextView(this);
+                p1Score.setLayoutParams(param);
+                p1Score.setText(Integer.toString(p1));
+                p1Score.setGravity(Gravity.CENTER);
+                p1Score.setTextSize(20);
+                column1.addView(p1Score);
+
+                column2 = (LinearLayout) findViewById(R.id.Column2);
+                p2Score = new TextView(this);
+                p2Score.setLayoutParams(param);
+                p2Score.setText(Integer.toString(p2));
+                p2Score.setGravity(Gravity.CENTER);
+                p2Score.setTextSize(20);
+                column2.addView(p2Score);
+
+                column3 = (LinearLayout) findViewById(R.id.Column3);
+                p3Score = new TextView(this);
+                p3Score.setLayoutParams(param);
+                p3Score.setText(Integer.toString(p3));
+                p3Score.setGravity(Gravity.CENTER);
+                p3Score.setTextSize(20);
+                column3.addView(p3Score);
+
+                // Add the scores to current scores
+                playerOneScore += p1;
+                playerTwoScore += p2;
+                playerThreeScore += p3;
+
+
+                // Make the fields blank for the next input
+                player1Score.setText("");
+                player2Score.setText("");
+                player3Score.setText("");
+
+                break;
+
+            case 4:
+
+                //get the numbers from the text boxes
+                player1Score = (EditText) findViewById(R.id.editText7);
+                p1 = Integer.parseInt(player1Score.getText().toString());
+                player2Score = (EditText) findViewById(R.id.editText8);
+                p2 = Integer.parseInt(player2Score.getText().toString());
+                player3Score = (EditText) findViewById(R.id.editText9);
+                p3 = Integer.parseInt(player3Score.getText().toString());
+                player4Score = (EditText) findViewById(R.id.editText10);
+                p4 = Integer.parseInt(player4Score.getText().toString());
+
+                // Add the scores into the view
+                column1 = (LinearLayout) findViewById(R.id.Column1);
+                p1Score = new TextView(this);
+                p1Score.setLayoutParams(param);
+                p1Score.setText(Integer.toString(p1));
+                p1Score.setGravity(Gravity.CENTER);
+                p1Score.setTextSize(20);
+                column1.addView(p1Score);
+
+                column2 = (LinearLayout) findViewById(R.id.Column2);
+                p2Score = new TextView(this);
+                p2Score.setLayoutParams(param);
+                p2Score.setText(Integer.toString(p2));
+                p2Score.setGravity(Gravity.CENTER);
+                p2Score.setTextSize(20);
+                column2.addView(p2Score);
+
+                column3 = (LinearLayout) findViewById(R.id.Column3);
+                p3Score = new TextView(this);
+                p3Score.setLayoutParams(param);
+                p3Score.setText(Integer.toString(p3));
+                p3Score.setGravity(Gravity.CENTER);
+                p3Score.setTextSize(20);
+                column3.addView(p3Score);
+
+                column4 = (LinearLayout) findViewById(R.id.Column4);
+                p4Score = new TextView(this);
+                p4Score.setLayoutParams(param);
+                p4Score.setText(Integer.toString(p4));
+                p4Score.setGravity(Gravity.CENTER);
+                p4Score.setTextSize(20);
+                column4.addView(p4Score);
+
+                // Add the scores to current scores
+                playerOneScore += p1;
+                playerTwoScore += p2;
+                playerThreeScore += p3;
+                playerFourScore += p4;
+
+                // Make the fields blank for the next input
+                player1Score.setText("");
+                player2Score.setText("");
+                player3Score.setText("");
+                player4Score.setText("");
+
+                break;
+        }
+
+
     }
 
     /**
@@ -109,8 +274,13 @@ public class TrackScore extends AppCompatActivity {
         intent.putExtra("Number of Players", numPlayers);
         intent.putExtra("PlayerOne Score", playerOneScore);
         intent.putExtra("PlayerTwo Score", playerTwoScore);
+        intent.putExtra("PlayerThree Score", playerThreeScore);
+        intent.putExtra("PlayerFour Score", playerFourScore);
+
         intent.putExtra("PlayerOne Name", player1);
         intent.putExtra("PlayerTwo Name", player2);
+        intent.putExtra("PlayerThree Name", player3);
+        intent.putExtra("PlayerFour Name", player4);
 
         // Start the second activity
         startActivity(intent);
